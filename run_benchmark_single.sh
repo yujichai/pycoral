1dd3a2ab9e0bd7ad9421d63338f696e2bccf167c
yujichai <yujichai@gmail.com>
2022-05-01 00:08:30 -0400

    benchmark single debugging

--- run_benchmark_single.sh
+++ run_benchmark_single.sh
@@ -1,16 +1,16 @@
 DATASET=kws_chu_july19 #ssd_300_reduced cifar10_july8 kws_chu_july19 imagenet_may26 vit_cifar100
-INPUT_MODEL_PATH=/home/mendel/disk-USB/model-c/$DATASET
+INPUT_MODEL_DIR=/home/mendel/disk-USB/model-c/$DATASET
 OUTPUT_DATASET_LOG_DIR=/home/mendel/disk-USB/log-EdgeTPU/$DATASET
 WARM_COUNT=3
 COUNT=10
 PRINT_INTERVAL=1000
 
 COUNTER=0
-for f in $INPUT_DATASET_DIR/*; do
+for f in $INPUT_MODEL_DIR/*; do
     echo $f
     MODEL_PATH=$f
     if [ -f "$MODEL_PATH" ]; then
-        python3 examples/benchmark_tflite_single.py -i $INPUT_MODEL_PATH -o $OUTPUT_DATASET_LOG_DIR -w $WARM_COUNT -c $COUNT 
+        python3 examples/benchmark_tflite_single.py -i $f -o $OUTPUT_DATASET_LOG_DIR -w $WARM_COUNT -c $COUNT 
         let COUNTER=COUNTER+1 
     else 
         echo "$MODEL_PATH does not exist."
