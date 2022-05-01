a0d84dd836fb4fa246d79944684d257cb1e55b8e
yujichai <yujichai@gmail.com>
2022-04-30 23:11:12 -0400

    local changes

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,4 +1,4 @@
-DATASET=kws_chu_july19 #ssd_300_reduced cifar10_july8 kws_chu_july19 imagenet_may26 vit_cifar100
+DATASET=imagenet_may26 #ssd_300_reduced cifar10_july8 kws_chu_july19 imagenet_may26 vit_cifar100
 INPUT_MODEL_DIR=/home/mendel/disk-USB/model-c
 OUTPUT_LOG_DIR=/home/mendel/disk-USB/log-EdgeTPU
 WARM_COUNT=3
@@ -6,7 +6,7 @@ COUNT=10
 PRINT_INTERVAL=1000
 BREAK_FLAG=0
 START_INDEX=0
-END_INDEX=2000
+END_INDEX=100000
 
 python3 examples/benchmark_tflite.py \
 -d $DATASET \
